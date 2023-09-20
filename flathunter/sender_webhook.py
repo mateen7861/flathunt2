731e7039cff6862aa4c1ab5f1a42f1c6e22415a4
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2023-09-20 13:44:23 +0500

    Update sender_webhook.py

--- flathunter/sender_webhook.py
+++ flathunter/sender_webhook.py
@@ -10,22 +10,61 @@ from flathunter.config import YamlConfig
 class SenderWebhook(Processor, Notifier):
 		def __init__(self, config: YamlConfig, receivers=None):
 				self.config = config
-				self.api_key = self.config.webhook_api_key()
-				self.__webhook_url = "https://5773-39-40-178-230.ngrok.io/webhook"
+				self.api_key = self.config.whatsapp_api_key()
 
-		
+				self.__text_message_url = "https://5baa-39-40-166-132.ngrok.io/webhook"
+				self.__send_seen_url = "https://5baa-39-40-166-132.ngrok.io/webhook"
+				self.__start_typing_url = "https://5baa-39-40-166-132.ngrok.io/webhook"
+				self.__stop_typing_url = "https://5baa-39-40-166-132.ngrok.io/webhook"
+
+				if receivers is None:
+						self.receiver_ids = self.config.whatsapp_receiver_ids()
+				else:
+						self.receiver_ids = receivers
+
+		def process_expose(self, expose):
+				self.__broadcast(
+						receivers=self.receiver_ids,
+						message=self.__get_text_message(expose),
+				)
+				return expose
+
+		def __broadcast(self, receivers: List[int], message: str) -> None:
+				for receiver in receivers:
+						self.__send_text(receiver, message)
+
 		def notify(self, message: str):
+				self.__broadcast(self.receiver_ids, message, None)
+
+		def __send_text(self, chat_id: int, message: str) -> Dict:
 				payload = {
+					"chatId": f"{chat_id}@c.us",
 					"text": message,
 					"session": "default"
 				}
 
+				presencePayload = {
+					"chatId": f"{chat_id}@c.us",
+					"session": "default"
+				}
 				
 				headers={'X-API-KEY': self.api_key}
 
-				requests.request("POST", self.__webhook_url, data=payload, timeout=30, headers=headers)
+
+				requests.request("POST", self.__send_seen_url, data=presencePayload, timeout=30, headers=headers)
+				requests.request("POST", self.__start_typing_url, data=presencePayload, timeout=30, headers=headers)
+				requests.request("POST", self.__stop_typing_url, data=presencePayload, timeout=30, headers=headers)
+				requests.request("POST", self.__text_message_url, data=payload, timeout=30, headers=headers)
 
 				return {}
-				
 
-	
+		def __get_text_message(self, expose: Dict) -> str:
+				return self.config.message_format().format(
+						title=expose.get('title', 'N/A'),
+						rooms=expose.get('rooms', 'N/A'),
+						size=expose.get('size', 'N/A'),
+						price=expose.get('price', 'N/A'),
+						url=expose.get('url', 'N/A'),
+						address=expose.get('address', 'N/A'),
+						durations=expose.get('durations', 'N/A')
+				).strip()
